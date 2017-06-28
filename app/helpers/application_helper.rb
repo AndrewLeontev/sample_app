4a90d6650f3afd65662608f5c248de020aa815cc
Andrew Leontev <leontev.andrey.s@gmail.com>
2017-06-28 15:29:45 +1000

    add comments in  aplication_helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#ВОзвращает полный заголовок зависящий от страницы
+	def full_title(page_title)								#определение метода
+		base_title = "Ruby on Rails Tutorial Sample App"	#назначение переменной
+		if page_title.empty?								#булевый тест
+			base_title										#Явное возвращение
+		else
+			"#{base_title} | #{page_title}"					#Интерполяция строки
+		end
+	end
 end
